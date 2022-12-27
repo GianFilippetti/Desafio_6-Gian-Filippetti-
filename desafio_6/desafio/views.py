0d4737487d97c4ffa3291b4414bb9d05308712de
Gian Filippetti <Gian_filippetti_97hotmail.com>
2022-12-27 15:40:02 -0300

    Pude resolver lo de cargar a la base de datos , tengo que borrar elementos

--- desafio_6/desafio/views.py
+++ desafio_6/desafio/views.py
@@ -4,12 +4,12 @@ from desafio.models import familia
 
 # Create your views here.
 def modelo (request):
-    familia.objects.create (nombre = 'Anto' , edad= 29 , esta_casado =True)
+    familia.objects.create (nombre = 'Matilde' , edad= 57 , esta_casado = True)
     return HttpResponse ('Aca esta armado mi modelo')
 
 def lista_familiares (request):
     todos_los_familiares = familia.objects.all
-    
+    print  (todos_los_familiares)
     context = {
         'familiares': todos_los_familiares
     }
